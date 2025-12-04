1dfc3567f669008f6d7a169e89b8de98d059387e
hariharanr2024eee-nh <hariharan.r2024eee@sece.ac.in>
2025-12-04 06:07:25 +0530

    Add files via upload

---
+++ day3 mern/script.js
@@ -0,0 +1,29 @@
+function greet() {
+    console.log("Hello, World!");   
+}
+  greet();  
+  
+  let a=10;
+  let b=15;
+  let sum=a+b;
+  console.log("Sum is:",sum); 
+  let c=20;
+  let d=25;
+  let result=c+d;
+  console.log("Result is:"+result);  
+
+function add(){ 
+let a=10;
+let b=20;
+let sum=a+b;
+console.log("Addition is:",sum);
+} 
+add();
+{
+  function add(a, b){ 
+    let sum = a + b;
+    console.log("Sum is:", sum); 
+  }
+  add(10, 20);
+}  
+
